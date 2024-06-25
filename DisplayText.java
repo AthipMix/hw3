5fdbecabd1a2405ea5b887c97e836cc6475d32d6
AthipMix <674259028@webmail.npru.ac.th>
2024-06-25 11:38:22 +0700

    my first program

---
+++ DisplayText.java
@@ -0,0 +1,8 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Athip" + "Poosomboon");
+        System.out.println(20 + 8);
+        System.out.println("NPRU" + 2567);
+        System.out.println(0+ "2" + 8);
+    }
+}
