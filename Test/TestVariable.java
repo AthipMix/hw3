367519de21892291cae412b72afd929cf3ce12f1
AthipMix <674259028@webmail.npru.ac.th>
2024-07-23 11:55:19 +0700

    testvariable

---
+++ Test/TestVariable.java
@@ -0,0 +1,20 @@
+//package Test;
+public class TestVariable {
+    public static void main(String[] args) {
+        // 1) data type char
+        // char clothingSize = 's'; 
+        // System.out.println("clothingSize = "+clothingSize);
+
+        // 2) data type int
+        // int websiteVisitors = 0; 
+        // System.out.println("websiteVisitors = "+websiteVisitors);
+
+        // 3) data type char
+        // char Studentgrade = 'A';
+        // System.out.println("Studentgrade = "+Studentgrade);
+
+        // 4) data type float
+        float GPA = 4.00f;
+        System.out.println("GPA = "+GPA);
+    }
+}
