203a9d0e6a98c85822532d4bab21873397e4da01
AthipMix <674259028@webmail.npru.ac.th>
2024-07-30 12:17:24 +0700

    me

--- Celsius.java
+++ Celsius.java
@@ -1,6 +1,8 @@
 public class Celsius {
     public static void main(String[] args) {
-        
-        System.out.println();
+        double fahrenheit = 80;
+        double celsius = (5.0/9.0)*(fahrenheit-32);
+        System.out.println("fahrenhite"+fahrenheit);
+        System.out.println("celsius"+celsius);
     }
 }
