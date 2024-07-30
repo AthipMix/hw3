cae87c214c242dd710dc1c5d4ee9d9ce564d6721
AthipMix <674259028@webmail.npru.ac.th>
2024-07-30 12:02:58 +0700

    hw2

---
+++ Celsius.java
@@ -0,0 +1,6 @@
+public class Celsius {
+    public static void main(String[] args) {
+        
+        System.out.println();
+    }
+}
---
+++ hw-2/Average.java
@@ -0,0 +1,8 @@
+public class Average {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 8;
+        double avg = (num1 + num2)/2.0;
+        System.out.println(avg);
+    }
+}
