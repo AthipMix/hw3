f451a5ac87e000614625d7daf0d8a2b50dfd53cf
AthipMix <674259028@webmail.npru.ac.th>
2024-08-06 10:56:48 +0700

    Delete Celsius.java

--- Celsius.java
+++
@@ -1,8 +0,0 @@
-public class Celsius {
-    public static void main(String[] args) {
-        double fahrenheit = 80;
-        double celsius = (5.0/9.0)*(fahrenheit-32);
-        System.out.println("fahrenhite"+fahrenheit);
-        System.out.println("celsius"+celsius);
-    }
-}
