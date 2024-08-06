8720f03debdf0e3c1871b397235d3769abcacb05
AthipMix <674259028@webmail.npru.ac.th>
2024-08-06 10:56:18 +0700

    Delete hw-1 directory

--- hw-1/DisplayText.java
+++
@@ -1,8 +0,0 @@
-public class DisplayText {
-    public static void main(String[] args) {
-        System.out.println("Athip" + "Poosomboon");
-        System.out.println(20 + 8);
-        System.out.println("NPRU" + 2567);
-        System.out.println(0+ "2" + 8);
-    }
-}
